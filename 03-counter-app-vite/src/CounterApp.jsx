c6cef63c7f11229369761bbd9d5308a5ca197920
ClaudiaAriasH <claudia-ah@hotmail.com>
2023-09-06 20:13:04 -0500

    Events

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,20 @@
 import PropTypes from "prop-types";
 
 export const CounterApp = ({ title, value }) => {
+
+
+const handleAdd = (event, newValue)  => { 
+    console.log(event) 
+};
+
+
   return (
     <>
-      <h1>{title}.</h1>
-      <h2>Value: {value} </h2>
+      <h1> { title } </h1>
+      <h2> Value: { value } </h2>
+      <button onClick={ (event) => handleAdd(event, value)  }>
+        +1
+      </button>
     </>
   );
 };
@@ -15,6 +25,9 @@ CounterApp.propTypes = {
 };
 
 CounterApp.defaultProps = {
-  title: "Hola, Counter App",
+  title: "Hola, Counter App.",
   value: 1,
 };
+
+
+// Link eventos soportados por react https://es.legacy.reactjs.org/docs/events.html
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -5,6 +5,6 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CounterApp  title="Counter App" value={ 1 }/>
+    <CounterApp  title="Counter App." value={ 1 }/>
   </React.StrictMode>
 );
