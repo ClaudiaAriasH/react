9a3d93292007b5d1de09122807804cc5f00b106c
ClaudiaAriasH <claudia-ah@hotmail.com>
2023-10-03 16:51:11 -0500

    Custom hooks y archivos de barril

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,24 +1,19 @@
-import { getGifs } from "../helpers/getGifs";
-import { useEffect, useState } from "react";
-import { GifItem } from "./GifItem";
+import { GifItem } from './GifItem';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
 
-  const getImages = async () => {
-    const newImages = await getGifs(category);
-    setImages(newImages);
-  };
+    const { images, isLoading } = useFetchGifs( category );
+    
+    return (
+        <>
+            <h3>{ category }</h3>
+            {
+                isLoading && ( <h2>Cargando...</h2> )
+            }
+            
 
-  useEffect(() => {
-    getImages();
-  }, []);
-
-  return (
-    <>
-      <h3>{category}</h3>
-
-      <div className="card-grid">
+            <div className="card-grid">
                 {
                     images.map( ( image ) => (
                         <GifItem 
@@ -29,6 +24,7 @@ export const GifGrid = ({ category }) => {
                 }
                 
             </div>
-    </>
-  );
-};
+
+        </>
+    )
+}
