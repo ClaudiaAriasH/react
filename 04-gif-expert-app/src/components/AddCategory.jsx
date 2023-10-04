6a66badc5c32c1df633b08fe6ca8101f1b9a0981
ClaudiaAriasH <claudia-ah@hotmail.com>
2023-10-03 20:12:05 -0500

    Pruebas component AddCategory, simular submit, y Jest functions

--- 04-gif-expert-app/src/components/AddCategory.jsx
+++ 04-gif-expert-app/src/components/AddCategory.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from 'prop-types';
 
 export const AddCategory = ({ onNewCategory }) => {
   const [inputValue, setInputValue] = useState('');
@@ -17,7 +18,7 @@ export const AddCategory = ({ onNewCategory }) => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} aria-label="form">
       <h1>Add Category</h1>
       <input
         type="text"
@@ -28,3 +29,8 @@ export const AddCategory = ({ onNewCategory }) => {
     </form>
   );
 };
+
+
+AddCategory.propTypes = {
+  onNewCategory: PropTypes.func.isRequired,
+}
