06015b0b31d48c57ba80f1dc5359640428a7cff2
ClaudiaAriasH <claudia-ah@hotmail.com>
2023-10-06 19:27:40 -0500

    useReducer Hook y Agregar un nuevo TODO

---
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -0,0 +1,52 @@
+import { useReducer } from "react";
+import { todoReducer } from "./todoReducer";
+import { TodoList } from "./TodoList";
+import { TodoAdd } from "./TodoAdd";
+
+const initialState = [
+  {
+    id: new Date().getTime(),
+    description: "Recolectar la gema del alma",
+    done: false,
+  },
+  {
+    id: new Date().getTime() * 3,
+    description: "Recolectar la gema del poder",
+    done: false,
+  },
+];
+
+export const TodoApp = () => {
+  const [todos, dispatch] = useReducer(todoReducer, initialState);
+
+  const handleNewTodo = ( todo ) => {
+    const action = {
+        type: '[TODO] Add Todo',
+        payload: todo
+    }
+
+    dispatch( action );
+}
+
+  return (
+    <>
+      <h1>
+        TodoApp: 10, <small>pendientes: 10 </small>
+      </h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-7">
+            <TodoList  todos={ todos }/>
+
+        </div>
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr />
+          <TodoAdd onNewTodo={ handleNewTodo }/>
+
+        </div>
+      </div>
+    </>
+  );
+};
