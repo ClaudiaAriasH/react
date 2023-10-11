375ea3ee51102c568f61df4144906ef0d1e3614f
ClaudiaAriasH <claudia-ah@hotmail.com>
2023-10-11 16:41:25 -0500

    CreateContext y ContextProvider

--- 05-hook-app/src/09-useContext/MainApp.jsx
+++ 05-hook-app/src/09-useContext/MainApp.jsx
@@ -3,10 +3,11 @@ import { HomePage } from "./HomePage"
 import { AboutPage } from "./AboutPage"
 import { LoginPage } from "./LoginPage"
 import { NavBar } from "./NavBar"
+import { UserProvider } from "./context/UserProvider"
 
 export const MainApp = () => {
     return (
-        <>
+        <UserProvider>
             <NavBar/>
             <hr />
 
@@ -19,7 +20,7 @@ export const MainApp = () => {
                 <Route path="/*" element={<Navigate to="/about" />} />
 
             </Routes>
-        </>
+        </UserProvider>
 
     )
 }
---
+++ 05-hook-app/src/09-useContext/context/UserContext.jsx
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+
+//Permite conocer la información y definir el provider
+export const UserContext = createContext();
